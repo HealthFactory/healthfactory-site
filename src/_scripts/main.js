0aeaa5652c3ab66d7ce1cef2082f793182bc1a79
Sebastien Letelie <sebastien.letelie@gmail.com>
2016-07-29 16:12:37 +0200

    add pages

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -4,7 +4,7 @@ var $ = require('jquery');
 var jQuery = $;
 var ScrollMagic = require('scrollmagic');
 var TweenMax = require('gsap');
-require('scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js');
+var gsap = require('scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js');
 
 String.prototype.toCamel = function(){
 return this.replace(/([-_][a-z])/g, function($1){return $1.toUpperCase().replace(/[-_]/,'');});
@@ -952,3 +952,55 @@ function addLoadEvent(func) {
         }
     }
 }
+
+var loadGoogleMapsAPI = require('load-google-maps-api').default;
+
+loadGoogleMapsAPI({key: 'AIzaSyDq2L89c5GNfbf95yEZ-IIGz4ZfKI8uRf8'}).then(function(googleMaps) {
+  console.log(googleMaps); //=> Object { Animation: Object, ... 
+        var markers = [
+            ['Strasbourg', 48.57340529999999,7.752111300000024],
+            ['Montréal', 45.5016889,-73.56725599999999],
+            ['New-York', 40.7127837,-74.00594130000002]
+        ];
+
+        //map.fitBounds(bounds);
+
+        var map = document.getElementById('map');
+        var map = new googleMaps.Map(map, {
+            center: {
+                lat:44.9763176,
+                lng:-32.1239327
+            },
+            zoom: 3,
+            disableDefaultUI: true,
+        });
+
+        map.setOptions({
+            draggable: false,
+            zoomControl: false,
+            scrollwheel: false,
+            disableDoubleClickZoom: true
+        });
+
+        for(var i = 0; i < markers.length; i++ ) {
+            var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+            //bounds.extend(position);
+            var marker = new googleMaps.Marker({
+                position: position,
+                map: map,
+                icon: 'http://test.healthfactory.io/images/marker.png',
+                title: markers[i][0]
+            });
+        }
+
+
+        /*for( i = 0; i < markers.length; i++ ) {
+            addMarker(markers[i])
+        }*/
+
+
+        map.set('styles', [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"},{"gamma":"1.00"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"administrative","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative.country","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative.country","elementType":"labels.text","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"visibility":"simplified"},{"saturation":"-100"},{"lightness":"30"}]},{"featureType":"administrative.neighborhood","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"simplified"},{"gamma":"0.00"},{"lightness":"74"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#36393c"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"lightness":"3"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#4c4c4c"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2f3134"},{"weight":"0.01"}]}]);
+
+}).catch(function(err) {
+  console.error(err);
+});
