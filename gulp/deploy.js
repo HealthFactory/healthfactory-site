887db3ac204c91753595a034c07ea01702fd0a99
Sebastien Letelie <sebastien.letelie@gmail.com>
2016-08-11 22:09:28 +0200

    updates

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -1,9 +1,7 @@
 'use strict';
 
-var sftp = require('gulp-sftp');
-var changed = require('gulp-changed');
-var ghPages = require('gulp-gh-pages');
 var path = require('path');
+var SSH = require('gulp-ssh');
 
 module.exports = function (gulp, plugins, args, config, taskTarget, browserSync) {
   var dirs = config.directories;
@@ -13,17 +11,34 @@ module.exports = function (gulp, plugins, args, config, taskTarget, browserSync)
   var sshKey = '/Users/sebmade/.ssh/hhgandi_rsa';
   var vhost = args.production ? 'healthfactory.io' : 'test.healthfactory.io';
 
-  gulp.task('deploy', function () {
-    return gulp.src([path.join(taskTarget, '**/*'), '!'+path.join(taskTarget, 'data/')])
-      .pipe(ghPages())
-      .pipe(gulp.src(taskTarget+'/data/*'))
-      .pipe(gulp.changed('.'+taskTarget+'/data/*'))
-      .pipe(sftp({
+  gulp.task('deploy', function() {
+    return gulp.start('sftp');
+  })
+
+  gulp.task('ssh', function () {
+    var config = {host: 'git.dc0.gpaas.net', user: user, privateKey: require('fs').readFileSync(sshKey)};
+    var s = new SSH({ignoreErrors: false, sshConfig: config});
+    return s.exec(['deploy '+vhost+'.git gh-pages'], {filePath: 'commands.log'})
+      .pipe(gulp.dest('logs'))
+      .on('end', function() {
+        s.close();
+      });
+  });
+
+  gulp.task('push', function () {
+    return gulp.src([path.join('build', '/**'), '!'+path.join('build', '/htdocs/data/**')])
+      .pipe(plugins.ghPages());
+  });
+
+  gulp.task('sftp', function () {
+    return gulp.src(path.join(taskTarget,'/**'))
+      .pipe(plugins.changed('.'+taskTarget))
+      .pipe(gulp.dest('.'+taskTarget))
+      .pipe(plugins.sftp({
         host: 'sftp.dc0.gpaas.net',
         user: user,
         key: sshKey,
-        remotePath: 'vhosts/' + vhost + '/htdocs/data'
+        remotePath: 'vhosts/' + vhost + '/htdocs'
       }));
   });
-
 };
