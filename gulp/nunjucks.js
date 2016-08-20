450567e9d35b5937dc18ce9e1610896a37a5a201
Sebastien Letelie <sebastien.letelie@gmail.com>
2016-08-20 12:12:04 +0200

    start en translation

--- gulp/nunjucks.js
+++ gulp/nunjucks.js
@@ -4,9 +4,53 @@ var fs = require('fs');
 var path = require('path');
 var foldero = require('foldero');
 var nunjucks = require('gulp-nunjucks-html');
-var I18nExtension = require("nunjucks-i18n")(nunjucks);
 var yaml = require('js-yaml');
 var Promise = require('bluebird');
+var _ = require("lodash");
+
+function I18nExtension(options) {
+  this.tags = ['i18n'];
+
+  options = _.defaults(options || {}, {
+    translations: {},
+    locale: "__locale__"
+  });
+
+  var translate = function(defaultText, textId, kwargs) {
+    kwargs = kwargs || {};
+
+    var locale = options.locale;
+    var text = (options.translations[locale] || {})[textId] || (options.translations[locale] || {})[defaultText] || defaultText;
+
+    // Replace arguments
+    _.each(kwargs, function(value, arg) {
+      text = text.replace(arg, value);
+    });
+
+    return text;
+  }
+
+  this.parse = function(parser, nodes, lexer) {
+    var tok = parser.nextToken();
+    var args = parser.parseSignature(null, true);
+    parser.advanceAfterBlockEnd(tok.value);
+    var body = parser.parseUntilBlocks('endi18n');
+    parser.advanceAfterBlockEnd();
+    return new nodes.CallExtension(this, 'run', args, [body]);
+  };
+
+  this.run = function(context, textId, kwargs) {
+    kwargs = kwargs || {};
+    body = _.last(arguments);
+
+    var text = translate.call(context, body(), textId, kwargs);
+    return new nunjucks.runtime.SafeString(text);
+  };
+
+  // Add filter
+  options.env.addFilter("i18n", translate);
+};
+
 
 module.exports = function(gulp, plugins, args, config, taskTarget, browserSync) {
   var dirs = config.directories;
@@ -26,12 +70,10 @@ module.exports = function(gulp, plugins, args, config, taskTarget, browserSync) 
           try {
             if (path.extname(file).match(/^.ya?ml$/)) {
               json = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
-            }
-            else {
+            } else {
               json = JSON.parse(fs.readFileSync(file, 'utf8'));
             }
-          }
-          catch(e) {
+          } catch (e) {
             console.log('Error Parsing DATA file: ' + file);
             console.log('==== Details Below ====');
             console.log(e);
@@ -53,55 +95,55 @@ module.exports = function(gulp, plugins, args, config, taskTarget, browserSync) 
     var generate = function(lang, langConfig, langDest) {
       return new Promise(function(resolve, reject) {
         gulp.src([
-          path.join(dirs.source, '**/*.njk'),
-          '!' + path.join(dirs.source, '{**/\_*,**/\_*/**}')
-        ])
-        .pipe(plugins.changed(langDest))
-        .pipe(plugins.plumber())
-        .pipe(plugins.data({
-          __locale__: lang,
-          config: config,
-          production: args.production ? true : false,
-          debug: true,
-          site: {
-            data: siteData
-          }
-        }))
-        .pipe(nunjucks({
-          searchPaths: [path.join(dirs.source)],
-          setUp: function(env) {
-            env.addExtension('I18nExtension', new I18nExtension({
-              // TODO : bug dans nunjucks-i18n, locale est pas repris dans this.ctx quand on est dans une macro 
-              locale: lang,
-              env: env,
-              translations: langConfig
-            }));
-            return env;
-          },
-          ext: '.html'
-        })
-        .on('error', function(err) {
-          plugins.util.log(err);
-        }))
-        .on('error', plugins.notify.onError(config.defaultNotification))
-        .on('error', reject)
-        .pipe(plugins.htmlmin({
-          collapseWhitespace: true,
-          collapseBooleanAttributes: true,
-          conservativeCollapse: true,
-          removeComments: true,
-          removeEmptyAttributes: true,
-          removeRedundantAttributes: true
-        }))
-        .pipe(gulp.dest(langDest))
-        //.pipe(browserSync.reload({stream: true, once: true}))
-        .on('end', resolve);
+            path.join(dirs.source, '**/*.njk'),
+            '!' + path.join(dirs.source, '{**/\_*,**/\_*/**}')
+          ])
+          .pipe(plugins.changed(langDest))
+          .pipe(plugins.plumber())
+          .pipe(plugins.data({
+            __locale__: lang,
+            config: config,
+            production: args.production ? true : false,
+            debug: true,
+            site: {
+              data: siteData
+            }
+          }))
+          .pipe(nunjucks({
+              searchPaths: [path.join(dirs.source)],
+              setUp: function(env) {
+                env.addExtension('I18nExtension', new I18nExtension({
+                  // TODO : bug dans nunjucks-i18n, locale est pas repris dans this.ctx quand on est dans une macro 
+                  locale: lang,
+                  env: env,
+                  translations: langConfig
+                }));
+                return env;
+              },
+              ext: '.html'
+            })
+            .on('error', function(err) {
+              plugins.util.log(err);
+            }))
+          .on('error', plugins.notify.onError(config.defaultNotification))
+          .on('error', reject)
+          .pipe(plugins.htmlmin({
+            collapseWhitespace: true,
+            collapseBooleanAttributes: true,
+            conservativeCollapse: true,
+            removeComments: true,
+            removeEmptyAttributes: true,
+            removeRedundantAttributes: true
+          }))
+          .pipe(gulp.dest(langDest))
+          //.pipe(browserSync.reload({stream: true, once: true}))
+          .on('end', resolve);
       })
     };
 
     Promise.all([
-      generate('fr', {fr: JSON.parse(fs.readFileSync('.lang/fr.json', 'utf8'))}, dest),
-      generate('en', {en: JSON.parse(fs.readFileSync('.lang/en.json', 'utf8'))}, dest+'/en/')
+      generate('fr', { fr: JSON.parse(fs.readFileSync('.lang/fr.json', 'utf8')) }, dest),
+      generate('en', { en: JSON.parse(fs.readFileSync('.lang/en.json', 'utf8')) }, dest + '/en/')
     ]).then(function() {
       browserSync.reload();
     });
